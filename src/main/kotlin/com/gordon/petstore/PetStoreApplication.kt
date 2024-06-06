7d94439f114152fdcba9cb9caf599b8e2d863d56
Gordon <gordon.velichkovski@proton.me>
2024-06-06 13:28:29 +0200

    PetStoreApplication

---
+++ src/main/kotlin/com/gordon/petstore/PetStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.gordon.petstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetStoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<PetStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/gordon/petstore/PetStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gordon.petstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetStoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
